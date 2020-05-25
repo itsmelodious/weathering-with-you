affa9472f0e16dcc6d120d61e4e1f4e5be91102d
Richard Wang <richardwang@Richards-Air-2.attlocal.net>
2020-05-25 03:06:09 -0700

    added basic js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+var endDate = new Date("May 27, 2020 00:00:00").getTime()
+
+var x = setInterval(
+    function() {
+        
+        var now = new Date().getTime();
+        var dif = endDate - now;
+
+        var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((dif % (1000 * 60)) / 1000);
+
+        //display days, hours, minutes, seconds
+
+        if (dif < 0) {
+            //if after the end date
+            //do something 
+        } else {
+            //display normally
+        }
+    }, 1000);
+)

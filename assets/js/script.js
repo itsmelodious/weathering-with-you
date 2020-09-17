315e2d8ac6fe4acd2b9d2db58aae1317df762d80
Melody Yan <itsmelodious@users.noreply.github.com>
2020-09-17 16:39:27 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,61 @@
 
 var endDate = new Date("September 15, 2020 00:00:00").getTime()
 
-var x = setInterval(
-    function() {
-        var now = new Date().getTime();
-        var dif = endDate - now;
+function fn() {
+    var now = new Date().getTime();
+    var dif = endDate - now;
 
-        var days = Math.floor(dif / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((dif % (1000 * 60)) / 1000);
-        
-        var header = "Coming Soon";
-        var timing = "until";
+    var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((dif % (1000 * 60)) / 1000);
 
-        // if after the end date
-        // display the days, hours, minutes, seconds without the negative sign
-        // adjust text to reflect past tense
-        if (dif <= 0) {
-            days *= -1;
-            hours *= -1;
-            minutes *= -1;
-            seconds *= -1;
-            header = "Now Playing";
-            timing = "since";
-        }
-        
-        // set header
-        document.getElementById("description-header").innerHTML = header;
-        
-        // set description text
-        var descriptionText = `Countdown ${timing} the U.S. DVD release of Weathering With You.`;
-        document.getElementById("countdown-description").innerHTML = descriptionText;
-        
-        // display days, hours, minutes, seconds
-        document.getElementById("days").innerHTML = days;
+    var header = "Coming Soon";
+    var timing = "until";
 
-        // set number of hours
-        if (hours < 10) {
-            document.getElementById("hours").innerHTML = "0" + hours;
+    // if after the end date
+    // display the days, hours, minutes, seconds without the negative sign
+    // adjust text to reflect past tense
+    if (dif <= 0) {
+        days *= -1;
+        hours *= -1;
+        minutes *= -1;
+        seconds *= -1;
+        header = "Now Playing";
+        timing = "since";
+    }
 
-        } else {
-            document.getElementById("hours").innerHTML = hours;
-        }
-        // set number of minutes
-        if (minutes < 10) {
-            document.getElementById("minutes").innerHTML = "0" + minutes;
+    // set header
+    document.getElementById("description-header").innerHTML = header;
 
-        } else {
-            document.getElementById("minutes").innerHTML = minutes;
-        }
+    // set description text
+    var descriptionText = `Countdown ${timing} the U.S. DVD release of Weathering With You.`;
+    document.getElementById("countdown-description").innerHTML = descriptionText;
 
-        if (seconds < 10) {
-            document.getElementById("seconds").innerHTML = "0" + seconds;
+    // display days, hours, minutes, seconds
+    document.getElementById("days").innerHTML = days;
 
-        } else {
-            document.getElementById("seconds").innerHTML = seconds;
-        }
-    }, 1000);
+    // set number of hours
+    if (hours < 10) {
+        document.getElementById("hours").innerHTML = "0" + hours;
+
+    } else {
+        document.getElementById("hours").innerHTML = hours;
+    }
+    // set number of minutes
+    if (minutes < 10) {
+        document.getElementById("minutes").innerHTML = "0" + minutes;
+
+    } else {
+        document.getElementById("minutes").innerHTML = minutes;
+    }
+
+    if (seconds < 10) {
+        document.getElementById("seconds").innerHTML = "0" + seconds;
+
+    } else {
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+}
+
+setinterval(fn, 1000);
